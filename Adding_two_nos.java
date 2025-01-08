2804aa34031b2a8fb1e3fd84589a7616df97694a
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-08 15:06:30 +0530

    Moved .class files from master to Day-01 branch

--- Adding_two_nos.java
+++ Adding_two_nos.java
@@ -1,10 +1,10 @@
-public class {
+public class Adding_two_nos{
 	public static void main(String[] args){
-		int a ;
-		int b ;
+		int a = 5 ;
+		int b = 6  ;
 		int result;
 		
-		result = a + b;
+		result = (a + b);
 		System.out.println("result" + result);
 	}
 } 
