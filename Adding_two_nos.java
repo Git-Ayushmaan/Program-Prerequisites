4ec944893a43d9e0d4630e28083b64b2cffc2691
Git-Ayushmaan <20ayushmaan@gmail.com>
2025-01-08 14:37:20 +0530

    .class file generted

---
+++ Adding_two_nos.java
@@ -0,0 +1,10 @@
+public class Adding_two_nos{
+	public static void main(String[] args){
+		int a = 5 ;
+		int b = 6  ;
+		int result;
+		
+		result = (a + b);
+		System.out.println("result" + result);
+	}
+} 
